804a060f5fd5200c2282a5db332b237300377f6e
Creg Antony <Creg@Cregantony.com>
2020-11-15 23:55:14 -0500

    changed some metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,8 +2,8 @@ require('dotenv').config()
 
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `Creg Antony Digital Product Consultant`,
+    description: `You are watching a master at work`,
     author: `@gatsbyjs`,
   },
   plugins: [
